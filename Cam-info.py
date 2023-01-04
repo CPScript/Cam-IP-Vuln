cef60c2694dcba561305af1c715ab8975d228959
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-04 08:55:34 -0500

    Create Cam-info.py

---
+++ Cam-info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+import requests,re,os
+import time
+import sys
+from os import system
+from platform import platform
+from time import sleep
+import os
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+
+os.system(delet)
+time.sleep(1)
+
+
+print("""
+
+CPScript is nt responsible 4 ur acctions :P
+
+"""")
